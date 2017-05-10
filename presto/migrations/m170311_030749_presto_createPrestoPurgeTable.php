044083ed19031227457f78dc60bdf0e8dc803cc2
Ryan Payne <rpayne@lewiscommunications.com>
2017-05-10 12:37:49 -0500

    Update rootPath in migration

--- presto/migrations/m170311_030749_presto_createPrestoPurgeTable.php
+++ presto/migrations/m170311_030749_presto_createPrestoPurgeTable.php
@@ -8,12 +8,20 @@ class m170311_030749_presto_createPrestoPurgeTable extends BaseMigration
 	 */
 	public function safeUp()
 	{
-		craft()->db->createCommand()->createTable('presto_cachepurge', [
+		$command = craft()->db->createCommand();
+		$command->createTable('presto_cachepurge', [
 			'purgedAt' => ['column' => 'datetime', 'required' => true],
 			'paths' => ['required' => true],
 		], null, true);
+		$command->createIndex('presto_cachepurge', 'purgedAt', false);
 
-		craft()->db->createCommand()->createIndex('presto_cachepurge', 'purgedAt', false);
+		// Update rootPath setting
+		craft()->plugins->savePluginSettings(
+			craft()->plugins->getPlugin('Presto'), [
+				'cachePath' => craft()->request->getPost('cachePath'),
+				'rootPath' => craft()->config->get('rootPath', 'presto')
+			]
+		);
 
 		return true;
 	}
